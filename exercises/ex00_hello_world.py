08483c8af56442541214745bd1f57229cfeafa74
Sushant Potu <spotu3@gmail.com>
2024-08-26 09:15:21 -0400

    exercise 00 hello world

---
+++ exercises/ex00_hello_world.py
@@ -0,0 +1,14 @@
+print("Hello, world!")
+
+"""My first exercise in COMP110!"""
+
+__author__ = "730768373"
+
+
+def greet(name: str) -> str:
+    """A welcoming first function definition."""
+    return "Hello, " + name + "!"
+
+
+greet(name="Campers")
+greet(name="Kris")
